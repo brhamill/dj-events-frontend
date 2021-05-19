91c4c54264a635c93c0e7e725a17fe5507a58b65
Brad Hamill <brad@hamill.com>
2021-05-18 22:06:01 -0600

    Events Pagination

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,14 +1,19 @@
 import { Layout } from '@/components/Layout'
-import { API_URL } from '@/config/index'
+import { API_URL, PER_PAGE } from '@/config/index'
 import { FunctionComponent } from 'react'
 import { IEvent } from '@/interfaces/IEvent'
 import { EventItem } from '@/components/EventItem'
+import { Pagination } from '@/components/Pagination'
 
 type Props = {
   events: Array<IEvent>
+  page: number
+  total: number
 }
 
-const EventsPage: FunctionComponent<Props> = ({ events }) => {
+const EventsPage: FunctionComponent<Props> = ({ events, page, total }) => {
+  const lastPage = Math.ceil(total / PER_PAGE)
+
   return (
     <Layout>
       <h1>Events</h1>
@@ -17,17 +22,28 @@ const EventsPage: FunctionComponent<Props> = ({ events }) => {
       {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+
+      <Pagination page={page} total={total} />
     </Layout>
   )
 }
 
-export const getStaticProps = async () => {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`)
-  const events = await res.json()
+export const getServerSideProps = async ({ query: { page = 1 } }) => {
+  // Calculate start page
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE
+
+  // Fetch total/count
+  const totalRes = await fetch(`${API_URL}/events/count`)
+  const total = await totalRes.json()
+
+  // Fetch events
+  const eventRes = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  )
+  const events = await eventRes.json()
 
   return {
-    props: { events },
-    revalidate: 1,
+    props: { events, page: +page, total },
   }
 }
 
