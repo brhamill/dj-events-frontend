0ec6ebe7838895d76cb2a679a4ee35a14de54a1a
Brad Hamill <brad@hamill.com>
2021-05-19 18:08:10 -0600

    Gettting User Events For Dashboard

--- pages/account/dashboard.tsx
+++ pages/account/dashboard.tsx
@@ -1,11 +1,31 @@
 import { Layout } from '@/components/Layout'
+import { API_URL } from '@/config/index'
+import { parseCookies } from '@/helpers/index'
 
-const DashboardPage = () => {
+const DashboardPage = ({ events }) => {
+  console.log(events)
   return (
     <Layout title='User Dashboard'>
       <h1>Dashboard</h1>
     </Layout>
   )
+}
+
+export async function getServerSideProps({ req }: any) {
+  const { token } = parseCookies(req)
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const events = await res.json()
+
+  return {
+    props: { events },
+  }
 }
 
 export default DashboardPage
--- pages/events/edit/[id].tsx
+++ pages/events/edit/[id].tsx
@@ -191,8 +191,6 @@ export async function getServerSideProps({ params: { id }, req }: Params) {
   const res = await fetch(`${API_URL}/events/${id}`)
   const evt = await res.json()
 
-  console.log(req.headers.cookie)
-
   return {
     props: { evt },
   }
