919665f530c8e2a975d63bff471927c3e4708335
Brad Hamill <brad@hamill.com>
2021-05-19 19:37:31 -0600

    Associating Events With Users

--- pages/events/add.tsx
+++ pages/events/add.tsx
@@ -1,14 +1,16 @@
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { ChangeEvent, FormEvent, useState } from 'react'
+import { parseCookies } from '@/helpers/index'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { Layout } from '@/components/Layout'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Form.module.css'
 import { IEvent } from '@/interfaces/IEvent'
+import { parse } from 'qs'
 
-const AddEventPage = () => {
+const AddEventPage = ({ token }) => {
   const [values, setValues] = useState({
     name: '',
     performers: '',
@@ -37,11 +39,16 @@ const AddEventPage = () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(values),
     })
 
     if (!res.ok) {
+      if (res.status === 403 || res.status === 401) {
+        toast.error('No token included')
+        return
+      }
       toast.error('Something Went Wrong')
     } else {
       const evt: IEvent = await res.json()
@@ -139,6 +146,16 @@ const AddEventPage = () => {
       </form>
     </Layout>
   )
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req)
+
+  return {
+    props: {
+      token,
+    },
+  }
 }
 
 export default AddEventPage
